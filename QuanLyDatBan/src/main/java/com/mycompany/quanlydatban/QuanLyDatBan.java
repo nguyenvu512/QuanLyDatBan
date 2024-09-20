097402ab5b4394b79e29e61f45226c771423dd53
nguyenvu512 <vu5122004@gmail.com>
2024-09-20 13:41:21 +0700

    init

---
+++ QuanLyDatBan/src/main/java/com/mycompany/quanlydatban/QuanLyDatBan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlydatban;
+
+/**
+ *
+ * @author ACER
+ */
+public class QuanLyDatBan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
