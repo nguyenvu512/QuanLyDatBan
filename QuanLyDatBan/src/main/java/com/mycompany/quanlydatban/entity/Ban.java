d8daa02e495a97fbd9924e769c1783cf6821043d
nguyenvu512 <vu5122004@gmail.com>
2024-09-20 14:23:19 +0700

    download lombok

--- QuanLyDatBan/src/main/java/com/mycompany/quanlydatban/entity/Ban.java
+++ QuanLyDatBan/src/main/java/com/mycompany/quanlydatban/entity/Ban.java
@@ -4,10 +4,23 @@
  */
 package com.mycompany.quanlydatban.entity;
 
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 /**
  *
  * @author ACER
  */
+@Data // Tự động sinh getter, setter, toString, equals, và hashCode
+@NoArgsConstructor // Tự động sinh constructor không tham số
+@AllArgsConstructor // Tự động sinh constructor có tham số
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE) // tự động thêm private trước mỗi thuộc tính
 public class Ban {
-    private String maBan;
+    String maBan = UUID.randomUUID().toString();
 }
